9f93dad94ec4f44d2c30ca2e841fbc007a56df6b
user <themarquis@Mail.ru>
2024-04-27 22:36:45 +0300

    Закончил ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author1 = new Author("Роззи", "Сброззи");
+        Author author2 = new Author("Рэй", "Грэй");
+        Book book1 = new Book("Бубль Гум моего детства", author1, 1970);
+        Book book2 = new Book("451 градус моего терпения", author2, 1950);
+        book1.setPublicationYear(1955);
+        System.out.println(book1);
+        System.out.println(book2);
     }
 }
