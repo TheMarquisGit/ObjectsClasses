27364f1346e42caae6eeee4275b4bb095b883adb
user <themarquis@Mail.ru>
2024-04-27 22:39:07 +0300

    Закончил ДЗ и отформ код

--- src/Book.java
+++ src/Book.java
@@ -5,7 +5,7 @@ public class Book {
     private Author author;
     private int publicationYear;
 
-    public Book (String nameBook, Author author, int publicationYear) {
+    public Book(String nameBook, Author author, int publicationYear) {
         this.nameBook = nameBook;
         this.author = author;
         this.publicationYear = publicationYear;
@@ -14,12 +14,15 @@ public class Book {
     public String getNameBook() {
         return this.nameBook;
     }
+
     public Author getAuthor() {
         return this.author;
     }
+
     public int getPublicationYear() {
         return this.publicationYear;
     }
+
     public void setPublicationYear(int publicationYear) {
         if (publicationYear > LocalDate.now().getYear()) {
             System.out.println("В книге " + getNameBook() + " неверно указан год публикации " + publicationYear + " , этот год ещё не наступил");
@@ -27,6 +30,7 @@ public class Book {
         }
         this.publicationYear = publicationYear;
     }
+
     public String toString() {
         return "Название книги: " + nameBook + ", Автор: " + author + ", Год публикации: " + publicationYear;
     }
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         Author author2 = new Author("Рэй", "Грэй");
         Book book1 = new Book("Бубль Гум моего детства", author1, 1970);
         Book book2 = new Book("451 градус моего терпения", author2, 1950);
+
         book1.setPublicationYear(1955);
         System.out.println(book1);
         System.out.println(book2);
